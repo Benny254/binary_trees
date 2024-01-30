035736759b9d99488a989f3b845d8015e023ac5b
Benny254 <asshimbenny01@mail.com>
2024-01-30 13:53:36 -0800

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_nodes - To count the nodes with at least 1 child in a binary tree.
+ * binary_tree_nodes - Count the nodes with at least 1 child in a binary tree.
  * @tree: Pointer to the root node of the tree to count the number of nodes.
  *
  * Return: If tree is NULL, the function must return 0, else return node count.
